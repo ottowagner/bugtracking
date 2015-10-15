33b792ed84f22be6df2adf99a8f2407867c433df
ottowagner <Vr7nf5sv>
2015-10-15 22:40:39 +0200

    OW: fixed import.sql bug

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
-INSERT INTO Bug (TITLE, DESCRIPTION, STATE, AUTOR, DEVELOPER, LASTUPDATEDATE, CREATIONDATE)
-VALUES ('Ein neuer Fehler!', 'Hier schau mal hier ist ein BUG!', 'In Bearbeitung', 1, 1, '02.01.2015', '01.01.2015');
+INSERT INTO Bug (TITLE, DESCRIPTION, STATE, AUTOR, DEVELOPER, LASTUPDATEDATE, CREATIONDATE) VALUES ('Ein neuer Fehler!', 'Hier schau mal hier ist ein BUG!', 'In Bearbeitung', 1, 1, '02.01.2015', '01.01.2015');
 INSERT INTO USER (EMAIL, FIRSTNAME, LASTNAME, PASSWORD) VALUES ('otto-wagner@gmx.net', 'Otto', 'Wagner', 'test');
