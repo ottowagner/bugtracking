6a14818fcd5fc732f8bb29c0b3e94347dbaf5532
13190 <Johan.Ahrens@nordakademie.de>
2015-11-01 18:59:52 +0100

    JA: security

--- src/main/java/de/nordakademie/iaa/bugtracking/security/SecurityConfig.java
+++ src/main/java/de/nordakademie/iaa/bugtracking/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.nordakademie.iaa.bugtracking.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -33,17 +35,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and().formLogin()/**.loginPage("/index.html")*/.permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
         http
-                .csrf().disable()
-                .exceptionHandling()
-                    .authenticationEntryPoint(unauthorizedHandler)
-                    .and()
-                .formLogin()
-                    .successHandler(authSuccess)
-                    .failureHandler(authFailure)
-                .and()
-                .authorizeRequests()
-                    .antMatchers("/**")
-                        .permitAll();
+                .csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and().authorizeRequests()
+                .antMatchers(new String[]{"/*", "/partials/login.html", "/resources/**/*", "/js/*","/rest/users"}).permitAll()
+                .anyRequest().authenticated();
+
+
+        /*.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                .anyRequest().authenticated();*/
     }
 }
