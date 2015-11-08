fba8c5ca38432f193ac588b180883038ba078164
Otto <otto-wagner@gmx.net>
2015-11-08 21:03:36 +0100

    OW: Frontend exceptionHandling completed

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -65,7 +65,7 @@ application.factory('authHttpResponseInterceptor', ['$q', '$location', 'errorSer
         return {
             responseError: function (rejection) {
                 if (rejection.status === 401) {
-                    errorService.setError(rejection); //TODO Fehler korrekt abfangen!
+                    errorService.setError(rejection.message);
                     $location.path('/login');
                 }
                 return $q.reject(rejection);
